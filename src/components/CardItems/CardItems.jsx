280cfbf7cdc3328fd83bdb2afbb3baf7610f37db
Juan Manuel CastillO <juanmcastillo3@gmail.com>
2024-09-25 21:24:27 -0300

    Ajustes de diseño Cards

--- src/components/CardItems/CardItems.jsx
+++ src/components/CardItems/CardItems.jsx
@@ -9,24 +9,24 @@ import './CardItems.css';
 function CardItems(props) {
   return (
     <Col className="columna">
-      <Card style={{ width: "22rem" }}>
-        <Card.Img variant="top" src={props.image} />
-        <Card.Body>
-          <Card.Title>{props.name}</Card.Title> 
-          <h6 className="card-price">{props.precio}</h6>
-          <div className="card-buttons">
-            <Link className="btn btn-primary btn-lg" to={`/item/${props.id}`}>
-              Ver más
-            </Link>
-            <Button variant="outline-primary">
-              <h6>
-                <FaShoppingCart size="1rem" />
-              </h6>
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Card style={{ width: "18rem", maxHeight: "450px" }}> 
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title> 
+        <h6 className="card-price">{props.precio}</h6>
+        <div className="card-buttons">
+          <Link className="btn btn-primary btn-lg" to={`/item/${props.id}`}>
+            Ver más
+          </Link>
+          <Button variant="outline-primary">
+            <h6>
+              <FaShoppingCart size="1rem" />
+            </h6>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
+  </Col>
   );
 }
 
